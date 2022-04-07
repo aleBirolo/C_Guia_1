ea003b6628ceb58165524a209e261d5e7b4b8e07
Martin-Roberto-lecuona <79217570+Martin-Roberto-lecuona@users.noreply.github.com>
2022-04-07 10:24:18 -0300

    Add files via upload

---
+++ Vectores.h
@@ -0,0 +1,14 @@
+#ifndef VECTORES_H_INCLUDED
+#define VECTORES_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+
+void printVec (int *vec, unsigned ce);// size_t == unsigned
+int insertarEnOrden (int *vec, unsigned tam,int *ce, int dato);
+int insertarEnPos (int *vec, unsigned tam,int *ce, int pos,int dato);
+int insertarEnPosYCorrer (int *vec, unsigned tam,int *ce, int pos,int dato);
+
+int elimDato (int *vec,int *ce,int dato);
+int elimDatoTodasRep (int *vec,int *ce,int dato);
+
+#endif // VECTORES_H_INCLUDED
